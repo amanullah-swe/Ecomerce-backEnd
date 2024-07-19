400861db878f15cd43a41bf42526d70cb72a97ce
amanullah-swe <115720070+amanullah-swe@users.noreply.github.com>
2024-07-19 20:51:03 +0530

    issue fix

--- middleware/multerMidelWare.js
+++ middleware/multerMidelWare.js
@@ -3,7 +3,9 @@ import multer from "multer";
 import path from 'path';
 export const tempDir = 'tmp/';
 const storage = multer.diskStorage({
-    destination: "./tmp/",
+    destination: function (req, file, cb) {
+        cb(null, '/tmp');
+    },
     filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
